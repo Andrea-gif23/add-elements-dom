4943d1b83d8e5a59e99e8632b0898829ac26322f
Andrea <andreagutierrezromero98@gmail.com>
2024-11-07 19:47:55 +0100

    todo actualizado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
  // Aquí tu código
+
+const botonAgregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+
+botonAgregar.addEventListener('click', function() {
+  
+  const nuevoElemento = prompt('Introduce un nuevo elemento para la lista:');
+  
+
+  if (nuevoElemento && nuevoElemento.trim() !== '') {
+    
+    const li = document.createElement('li');
+    li.textContent = nuevoElemento;
+
+   
+    lista.appendChild(li);
+  } else {
+    
+    alert('Por favor, ingresa un valor válido.');
+  }
+});
